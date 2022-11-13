c8c2664ffadc3cc0f74891fb2c917b4d4a918e39
Ajo <17722952+jo-0@users.noreply.github.com>
2022-11-13 20:23:39 +0530

    Fix deposits and withdrawal

--- savings_bank/admin.py
+++ savings_bank/admin.py
@@ -1,10 +1,19 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from savings_bank.models import Account, Transaction
-
-
-admin.site.register(Transaction)
 
 
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ["user", "balance", "bank_name"]
+    list_display = ["user", "balance", "bank_name", "branch"]
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ["created_at", "from_account", "to_account", "amount", "status", "transaction_type"]
+    exclude = ("status",)
+
+    def save_model(self, request, obj, form, change) -> None:
+        from_account = Account.objects.filter(id=obj.from_account.id).first()
+        if from_account.balance < obj.amount:
+            messages.error(request=request, message="Not enough balance")
+        return super().save_model(request, obj, form, change)
--- savings_bank/models.py
+++ savings_bank/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, transaction
 from django.contrib.auth import get_user_model
 
 
@@ -26,13 +26,50 @@ class Transaction(models.Model):
         FAILED = "Failed", "Failed"
         PENDING = "Pending", "Pending"
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="date")
     from_account = models.ForeignKey(Account, on_delete=models.PROTECT, related_name="from_account")
     to_account = models.ForeignKey(Account, on_delete=models.PROTECT, related_name="to_account")
     amount = models.PositiveIntegerField()
-    note = models.CharField(max_length=48)
-    status = models.CharField(max_length=8, choices=TransactionStatus.choices, null=True)
+    note = models.CharField(max_length=48, null=True)
+    status = models.CharField(max_length=8, choices=TransactionStatus.choices, default=TransactionStatus.PENDING)
     transaction_type = models.CharField(max_length=12, choices=TransactionType.choices)
 
     class Meta:
         verbose_name_plural = "transactions"
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.pk:
+            from_account = Account.objects.filter(id=self.from_account.id).first()
+            to_account = Account.objects.filter(id=self.to_account.id).first()
+
+            if from_account is not None and to_account is not None:
+                # check if sending account has enough balance.
+                if from_account.balance < self.amount:
+                    return
+
+                # doing transactions on the same account
+                if from_account == to_account:
+                    with transaction.atomic():
+                        # if user withdraws
+                        if self.transaction_type == self.TransactionType.DEBIT:
+                            from_account.balance -= self.amount
+                            from_account.save()
+                        # if user deposits
+                        elif self.transaction_type == self.TransactionType.CREDIT:
+                            from_account.balance += self.amount
+                            from_account.save()
+                    self.status = self.TransactionStatus.SUCCESS
+                    return super().save(*args, **kwargs)
+
+                with transaction.atomic():
+                    # Deduct amount from sending account
+                    from_account.balance -= self.amount
+                    from_account.save()
+
+                    # Add new balance to account
+                    to_account.balance += self.amount
+                    to_account.save()
+
+                    self.status = self.TransactionStatus.SUCCESS
+
+        return super().save(*args, **kwargs)
